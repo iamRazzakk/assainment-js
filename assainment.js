497849bfdf13f34aab18430fb30a47529087f620
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-09 16:34:51 +0600

    first commit

---
+++ assainment.js
@@ -0,0 +1,68 @@
+// function cubeNumber(number) {
+//     const output = Math.pow(4, 3)
+//     return output
+// }
+
+
+// Problem 2
+// function matchFinder(string1, string2) {
+//     if(string1.includes(string2)){
+//         return true
+//     }else if(typeof string1 && string2 !==)
+//     else{
+//         return false
+//     }
+// }
+
+
+
+// problem 3
+function sortMaker(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr => 0) {
+            let reverseMaker = arr.reverse()
+            return reverseMaker
+        }
+    }
+}
+const names = [4,2]
+const total = sortMaker(names)
+console.log(total);
+
+
+// problem 4
+
+// const person = {
+//     street: 10,
+//     house: "15A",
+//     society: "Earth Perfect"
+// };
+
+// function findAddress(obj) {
+//     const value = Object.values(obj);
+
+//     if (obj.society === "" && obj.house === "") {
+//         value[1] = "__",
+//             value[2] = "__"
+//         return value;
+//     }else if(obj.house === ""){
+//         value[1] = "__"
+//         return value
+//     }
+//      else {
+//        return value
+//     }
+// }
+// findAddress(person)
+
+
+// Problem 5
+// function canPay(changeArray, totalDue) {
+//     const sum = changeArray.reduce((a, b) => a + b, 0);
+//     if (sum >= totalDue) {
+//         return true
+//     } else if (changeArray.length = [] || sum < totalDue) {
+//         return "This is not empty array" && false
+//     }
+// }
+// canPay([1, 5, 5], 10)
