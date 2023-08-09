f4216093e88aa8f61ac1d4e0aa2560b86de42c06
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-09 22:55:36 +0600

    git commit

--- assainment.js
+++ assainment.js
@@ -1,62 +1,59 @@
-// function cubeNumber(number) {
-//     const output = Math.pow(4, 3)
-//     return output
-// }
 
 
-// Problem 2
+function cubeNumber(number){
+    const num = number
+    const pow = 3
+    return Math.pow(num,pow)
+}
+
 // function matchFinder(string1, string2) {
 //     if(string1.includes(string2)){
 //         return true
-//     }else if(typeof string1 && string2 !==)
+//     }
 //     else{
 //         return false
 //     }
 // }
+// matchFinder("JavaScript","Code")
 
 
+// function sortMaker(arr) {
+//     if (arr[0] <= 0 || arr[1] <= 0) {
+//         return "invalid input"
+//     } else if (arr[0] > arr[1]) {
+//         return arr
+//     } else if (arr[1] > arr[0]) {
+//         let makeReverse = arr.reverse()
+//         return makeReverse
+//     } else if (arr[0] === arr[1]) {
+//         return "equal"
+//     }
+// }
 
-// problem 3
-function sortMaker(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr => 0) {
-            let reverseMaker = arr.reverse()
-            return reverseMaker
-        }
-    }
-}
-const names = [4,2]
-const total = sortMaker(names)
-console.log(total);
 
-
-// problem 4
 
 // const person = {
-//     street: 10,
 //     house: "15A",
 //     society: "Earth Perfect"
 // };
-
 // function findAddress(obj) {
-//     const value = Object.values(obj);
-
-//     if (obj.society === "" && obj.house === "") {
-//         value[1] = "__",
-//             value[2] = "__"
-//         return value;
-//     }else if(obj.house === ""){
-//         value[1] = "__"
-//         return value
+//     let streetValue = obj.street;
+//     let houseValue = obj.house;
+//     let societyValue = obj.society;
+//     if (streetValue === undefined) {
+//         streetValue = '__'
 //     }
-//      else {
-//        return value
+//     if (houseValue === undefined) {
+//         houseValue = '__'
 //     }
+//     if (societyValue === undefined) {
+//         societyValue = '__'
+//     }
+//     let allValue = streetValue + ',' + houseValue + ',' + societyValue;
+//     return allValue;
 // }
-// findAddress(person)
 
 
-// Problem 5
 // function canPay(changeArray, totalDue) {
 //     const sum = changeArray.reduce((a, b) => a + b, 0);
 //     if (sum >= totalDue) {
@@ -65,4 +62,3 @@ console.log(total);
 //         return "This is not empty array" && false
 //     }
 // }
-// canPay([1, 5, 5], 10)
